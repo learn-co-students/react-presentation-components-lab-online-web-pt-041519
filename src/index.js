8085cce587bbb5a833b653603f29e285a5eaaab2
Sarah <saranderson13@gmail.com>
2020-01-29 17:34:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+function handleColorChange(e) {
+  let color = "#" + Math.floor(Math.random() * 16777215).toString(16)
+  e.target.style.color = color
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleColorChange} />
   </div>,
   document.getElementById('root')
 );
