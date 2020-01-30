a81dd2fae7dd6fcb92b7c2baa66999fc7e446a4a
github username <github email address>
2020-01-29 16:09:46 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const handleColor = e => {
+  let colorString = "#" + Math.floor(Math.random() * 16777215).toString(16)
+  e.target.style.color = colorString
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick="Still happy" />
+    <SimplerComponent handleClick={handleColor} />
   </div>,
   document.getElementById('root')
 );
